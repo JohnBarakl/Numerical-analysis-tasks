3916debb5699e4a73a1ae71721491848430467db
Ioannis Baraklilis <imparakl@csd.auth.gr>
2020-12-16 13:51:31 +0200

    PA = LU working

--- Άσκηση 3/a_PAeqLU.py
+++ Άσκηση 3/a_PAeqLU.py
@@ -3,6 +3,7 @@ def swap_rows(matrix, row1, row2):
     matrix[row1] = matrix[row2]
     matrix[row2] = temp
 
+
 def pivot_matrix(matrix, pivot, P):
     max_row_element_pos = pivot
     for i in range(pivot + 1, len(matrix)):
@@ -12,10 +13,21 @@ def pivot_matrix(matrix, pivot, P):
     swap_rows(matrix, pivot, max_row_element_pos)
     swap_rows(P, pivot, max_row_element_pos)
 
-def LU(A, b):
+
+def matrix_vector_multiplication(matrix, vector):
+    result = [0 for i in range(len(vector))]
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            result[i] += matrix[i][j]*vector[j]
+
+    return result
+
+
+def PLU(A):
     P = []
     U = []
-    for rowNumber, row in enumerate(A): # Copy A in U
+    for rowNumber, row in enumerate(A):  # Copy A in U
         U.append([])
         P.append([])
         for element in row:
@@ -30,28 +42,58 @@ def LU(A, b):
         for other_row_num in range(row_number + 1, len(U)):
             U[other_row_num][row_number] = U[other_row_num][row_number] / U[row_number][row_number]  # multiplier
             for col_number in range(row_number + 1, len(U)):
-                U[other_row_num][col_number] = U[other_row_num][col_number] - (U[other_row_num][row_number])*U[row_number][col_number]
+                U[other_row_num][col_number] = U[other_row_num][col_number] - (U[other_row_num][row_number]) * \
+                                               U[row_number][col_number]
 
-    L = [[0 for i in range(len(U))] for j in range(len(U)) ]
+    L = [[0 for i in range(len(U))] for j in range(len(U))]
     for i in range(len(U)): L[i][i] = 1
     for i in range(1, len(U)):
-        for j in range(i//2+1):
+        for j in range(i // 2 + 1):
             L[i][j] = U[i][j]
             U[i][j] = 0
 
+    return P, L, U
 
 
+def solve_system(A, b):
+    P, L, U = PLU(A)
 
-    return U
+    Pb = matrix_vector_multiplication(P, b)
+
+    # Solve Ly = Pb
+    y = [0.0 for i in range(len(Pb))]
+
+    for i in range(len(L)):
+        y[i] = Pb[i]
+        for j in range(len(L)):
+            if j == i:
+                continue
+
+            y[i] -= L[i][j] * y[j]
+        y[i] = y[i] / L[i][i]
+
+    # Solve Ux = y
+    x = [0.0 for i in range(len(y))]
+
+    for i in range(len(U) - 1, -1, -1):
+        x[i] = y[i]
+        for j in range(len(U)):
+            if j == i:
+                continue
+
+            x[i] -= U[i][j] * x[j]
+        x[i] = x[i] / U[i][i]
+
+    return x
 
 
-m = LU(
+m = solve_system(
     [
         [2, 1, 5],
         [4, 4, -4],
         [1, 3, 1]
     ],
-    []
+    [5, 0, 6]
 )
 
 for i in m:
